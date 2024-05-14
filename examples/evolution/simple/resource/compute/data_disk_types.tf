4c978c1bd2433f5d62d95c83ecbaf4954dedfd31
Sergey Koporulin <117099582+serkopa@users.noreply.github.com>
2024-05-14 11:03:15 +0300

    Add files via upload

---
+++ examples/evolution/simple/resource/compute/data_disk_types.tf
@@ -0,0 +1,14 @@
+data "cloudru_evolution_disk_type" "disk_types" {
+  # NOTE: Это опциональный параметр
+  filter {
+    # NOTE: Это опциональный параметр
+    # availability_zone_id = "00000000-0000-0000-0000-000000000000"
+  }
+}
+
+locals {
+  demo_disk_types = [
+    for s in data.cloudru_evolution_disk_type.disk_types.resources : s if s.name == "SDS"
+  ]
+  demo_disk_type = local.demo_disk_types.0
+}
