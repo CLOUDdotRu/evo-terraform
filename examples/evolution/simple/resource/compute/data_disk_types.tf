3693c859018b352f33149a085192f5e3e93a0f93
Sergey Koporulin <117099582+serkopa@users.noreply.github.com>
2024-05-15 08:42:19 +0300

    Update data_disk_types.tf

--- examples/evolution/simple/resource/compute/data_disk_types.tf
+++ examples/evolution/simple/resource/compute/data_disk_types.tf
@@ -8,7 +8,7 @@ data "cloudru_evolution_disk_type" "disk_types" {
 
 locals {
   demo_disk_types = [
-    for s in data.cloudru_evolution_disk_type.disk_types.resources : s if s.name == "SDS"
+    for s in data.cloudru_evolution_disk_type.disk_types.resources : s if s.name == "SSD"
   ]
   demo_disk_type = local.demo_disk_types.0
 }
