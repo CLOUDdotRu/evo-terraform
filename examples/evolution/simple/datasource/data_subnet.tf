795726c6f151f36d01c2a5648386239dcaf88b7b
Sergey Koporulin <117099582+serkopa@users.noreply.github.com>
2024-05-15 08:38:54 +0300

    Update data_subnet.tf

--- examples/evolution/simple/datasource/data_subnet.tf
+++ examples/evolution/simple/datasource/data_subnet.tf
@@ -10,7 +10,7 @@ data "cloudru_evolution_subnet" "subnets" {
     # NOTE: это опциональный параметр
     # tag_ids = ["00000000-0000-0000-0000-000000000000"]
     # NOTE: это опциональный параметр
-    # name = "subnet-816839"
+    # name = ""
     # NOTE: это опциональный параметр
     # subnet_address = "10.1.2.0/24"
   }
