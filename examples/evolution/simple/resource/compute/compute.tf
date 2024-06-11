6d3bb86324b78dc71fe53f499b3325f0fed37ccd
Sergey Koporulin <117099582+serkopa@users.noreply.github.com>
2024-06-11 11:48:35 +0300

    Update compute.tf

--- examples/evolution/simple/resource/compute/compute.tf
+++ examples/evolution/simple/resource/compute/compute.tf
@@ -180,9 +180,6 @@ resource "cloudru_evolution_compute" "test_compute" {
   project {
     # NOTE: Это опциональный параметр, по умолчанию используется project_id указанный в секции provider
     id = "00000000-0000-0000-0000-000000000000"
-
-    # NOTE: Это вычисляемый параметр
-    # product_instance_id = "00000000-0000-0000-0000-000000000000"
   }
 
   # NOTE: Это обязательный параметр
